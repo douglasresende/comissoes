7201e6cbc6ea1399e34c6b26afaad6f75903ccfa
Douglas R Camargo <douglas@maxstudio.com.br>
2013-06-06 00:06:57 -0300

    First commit with rails 3.0 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comissoes::Application.config.secret_token = 'dab3809e24bb0fb3d777f9c13c8db75a98dc3a2d73a4983ab94f03f33a9b4fe8f502864ad78f327a4b22c236d2c1551288c03104324220cee6884c872dc90eb4'
