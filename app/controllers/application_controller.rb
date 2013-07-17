eb007010f90c0251c4a927d206533599a1950efd
Douglas R Camargo <douglas@maxstudio.com.br>
2013-07-16 23:40:20 -0300

    Adding I18n to project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   # http_basic_authenticate_with :name => "user", :password => "pass", :except => [:index, :show]
   # http_basic_authenticate_with :name => "user", :password => "pass", :only => :destroy
+  
+  before_filter :set_locale
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { :locale => I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Comissoes::Application.routes.draw do
-  resources :products
-  resources :clients
-  resources :symptoms
-  resources :users
+  LOCALES = /en|pt\-BR/
+  scope "(:locale)", :locale => LOCALES do
+    resources :products
+    resources :clients
+    resources :symptoms
+    resources :users
+  end
+
+  match '/:locale' => 'home#index', :locale => LOCALES
   root :to => "home#index"
 end
